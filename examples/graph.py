6c531be7f61245adfc484cb3df94922fb8594779
JaylinCLeeSumlin <jaylin92603@gmail.com>
2024-09-16 10:33:41 -0400

    Class example: Implement breadth first search

--- examples/graph.py
+++ examples/graph.py
@@ -1,12 +1,32 @@
+from collections import deque
+
 class Graph:
     def __init__(self) -> None:
         self.edges = {}
 
     def add_edges(self, from_node, to_node, weight):
-        
+
         # if the from node is not in the graph, add it with no following node connected
         if from_node not in self.edges:
             self.edges[from_node] = []
         
         # add a node to an existing node
         self.edges[from_node].append((to_node, weight))
+
+    def BFS(start_node, goal_node):
+        # Implement deque data structure for BFS
+        queue = deque([start_node])
+
+        # Keep track of visited nodes, use set to avoid duplicated visits
+        visited_nodes = set()
+
+        while queue:
+            # Use popleft() to implement LILO
+            current_node = queue.popleft()
+
+            # Return current node if it is the goal node
+            if current_node == goal_node:
+                return current_node
+            
+            # If current node is not the goal, add it to the set of visited nodes
+            visited_nodes.add(current_node)
